b5a9a2f97ed0b1a370ec9cc4d4830319bf58edd7
Luka Managadze <managaluka0@gmail.com>
2025-07-22 19:00:52 +0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Luka Managadze on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
