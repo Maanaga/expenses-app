b5f6fd4980fe62839be8419a992fcd8f58b8a49e
Luka Managadze <managaluka0@gmail.com>
2025-08-09 18:11:35 +0400

    Initial commit

---
+++ SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  iExpense
+//
+//  Created by Luka Managadze on 7/26/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
